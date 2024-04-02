d9a47d5f490c7705d0336400329f622c3b4f6c5a
KDsreeram <158125948+KDsreeram@users.noreply.github.com>
2024-04-01 21:31:48 -0700

    Create script.js

---
+++ lab-6/script.js
@@ -0,0 +1,6 @@
+function calculate() {
+    var input1 = document.getElementById('input1').value;
+    var input2 = document.getElementById('input2').value;
+    var result = input1 + input2;
+    document.getElementById('result').value = result;
+  }
